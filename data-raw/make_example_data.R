f65103c8955ad83b329332410a5b759d6145c507
annescharf <ascharf@ab.mpg.de>
2023-08-01 00:31:53 +0200

    adding examples class move2

--- data-raw/make_example_data.R
+++ data-raw/make_example_data.R
@@ -1,6 +1,7 @@
 library(raster)
 library(move)
-setwd("data-raw") 
+library(move2)
+setwd("data-raw")
 
 ## buffalo_env
 buffalo_env <- brick("env_buffalo.grd")
@@ -17,3 +18,13 @@ save(buffalo_utm, file = "../data/buffalo_utm.rda", compress = "xz")
 
 ## build docu
 system("export R_MAX_NUM_DLLS=614; cd ..; R -e 'devtools::document()'")
+
+#transform to move2
+
+load("./data/buffalo_ll.rda")
+buffalo_ll_mv2 <- mt_as_move2(buffalo_ll)
+save(buffalo_ll_mv2, file = "./data/buffalo_ll_mv2.rda", compress = "xz")
+
+load("./data/buffalo_utm.rda")
+buffalo_utm_mv2 <- mt_as_move2(buffalo_utm)
+save(buffalo_utm_mv2, file = "./data/buffalo_utm_mv2.rda", compress = "xz")
